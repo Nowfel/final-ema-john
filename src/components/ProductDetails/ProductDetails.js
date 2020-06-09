2ac96fe5f1de7c3186cdebceef730cde110c4465
nowfel <abdullahalmahfuj.cse@gmail.com>
2020-06-10 05:31:24 +0600

    Load single product from database to React Component

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import fakeData from '../../fakeData';
 import Product from '../Products/Products';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const ProductDetails = () => {
     const {productkey}=useParams();
-    const product=fakeData.find(pd=>pd.key===productkey);
+   const [product, setProduct] = useState(null);
+   useEffect(()=>{
+    fetch('http://localhost:4200/product/'+productkey)
+    .then(res => res.json())
+    .then(data=>{
+        setProduct(data)
+    });
+   },[])
     //console.log(product);
     return (
         <div>
-            <h2>coooming sooon</h2>
-            <Product product={product}
+            <h2>product details</h2>
+           { product && <Product product={product}
                      showAddToCart={false}
-                     ></Product>
+                     ></Product>}
         </div>
     );
 };
